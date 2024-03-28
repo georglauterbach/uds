ce0c2481b2db7faac91a2261ccd606fb221f81da
Georg Lauterbach <44545919+georglauterbach@users.noreply.github.com>
2024-03-28 16:38:39 +0100

    add alias for `ssh` command

--- files/home/.config/bash/80-aliases.sh
+++ files/home/.config/bash/80-aliases.sh
@@ -10,6 +10,10 @@ alias lsa='ls -a'
 
 alias gcs='git commit'
 
+# this alias is used to make `CTRL + Backspace`
+# work in SSH environments
+alias ssh='TERM=xterm-256color ssh'
+
 # `EDITOR` is defined in `10-setup.sh`
 # shellcheck disable=SC2139,SC2154
 alias v="${EDITOR}"
