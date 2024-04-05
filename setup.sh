787dedec6594f2f36a4bb71fa99185db218693d0
georglauterbach <44545919+georglauterbach@users.noreply.github.com>
2024-04-05 18:21:43 +0200

    restructure `setup.sh`

--- setup.sh
+++ setup.sh
@@ -1,5 +1,12 @@
 #! /usr/bin/env bash
 
+# shellcheck source=/dev/null
+if ! source <(wget -q -O- https://raw.githubusercontent.com/georglauterbach/libbash/main/modules/log.sh || :) \
+|| [[ $(type -t log || :) != 'function' ]]; then
+  echo -e "$(date --iso-8601='seconds' || :)  ERROR  setup.sh  --  Could not access GitHub - please run 'wget -q -O- https://raw.githubusercontent.com/georglauterbach/libbash/main/modules/log.sh' manually and resolve the errors" >&2
+  exit 2
+fi
+
 # shellcheck disable=SC2317
 function __log_unexpected_error() {
   local MESSAGE="unexpected error occurred :: script='${SCRIPT:-${0}}' | function='${1:-none (global)}'"
@@ -8,26 +15,68 @@ function __log_unexpected_error() {
 }
 
 # shellcheck disable=SC2034
-LOG_LEVEL=${LOG_LEVEL:-info}
+LOG_LEVEL=${LOG_LEVEL:-trace}
 SCRIPT='UDS Setup'
+INSTALL_GUI=true
+[[ ${*} == *--no-gui* ]] && INSTALL_GUI=false
+
 readonly GITHUB_RAW_URL='https://raw.githubusercontent.com/georglauterbach/uds/main/files/'
 readonly TMP_CHECK_FILE='/tmp/.uds_running'
+
 export DEBIAN_FRONTEND=noninteractive
 export DEBCONF_NONINTERACTIVE_SEEN=true
-export INSTALL_GUI=true
 
 trap '__log_unexpected_error "${FUNCNAME[0]:-}" "${BASH_COMMAND:-}" "${LINENO:-}" "${?:-}"' ERR
 trap 'rm -f ${TMP_CHECK_FILE}' EXIT
 set -eE -u -o pipefail
 shopt -s inherit_errexit
 
-# shellcheck source=/dev/null
-if ! source <(curl -qsSfL https://raw.githubusercontent.com/georglauterbach/libbash/main/modules/log.sh || :) \
-|| [[ $(type -t log || :) != 'function' ]]
-then
-  echo -e "$(date --iso-8601='seconds' || :)  ERROR  setup.sh  --  Could not access GitHub - please run 'curl -qsSfL https://raw.githubusercontent.com/georglauterbach/libbash/main/modules/log.sh' manually and resolve the errors" >&2
-  exit 2
-fi
+function __start() {
+  if [[ $(uname -m || :) != 'x86_64' ]]; then
+    log 'error' 'arm64 is not supported (APT sources are complicated for arm64)'
+    exit 1
+  fi
+
+  if [[ ${EUID} -ne 0 ]]; then
+    touch "${TMP_CHECK_FILE}"
+
+    if ${INSTALL_GUI}; then
+      log 'debug' 'Running user-specific setup'
+      gsettings set org.freedesktop.ibus.panel.emoji hotkey "[]" || :
+    fi
+
+    log 'debug' 'Acquiring correct permissions'
+    # shellcheck disable=SC2312
+    sudo env -                 \
+      USER="${USER}"           \
+      HOME="${HOME}"           \
+      LOG_LEVEL="${LOG_LEVEL}" \
+      bash "$(realpath -eL "${BASH_SOURCE[0]}")" "${@}"
+    exit
+  else
+    if [[ ! -f ${TMP_CHECK_FILE} ]]; then
+      log 'error' 'Do not run this script as root yourself'
+      exit 1
+    fi
+
+    if ${INSTALL_GUI}; then
+      log 'info' 'Configuring GUI'
+    else
+      log 'info' 'Not configuring GUI'
+    fi
+
+    main "${@}"
+  fi
+}
+
+function main() {
+  log 'info' 'Starting UDS setup process'
+  #purge_snapd
+  add_ppas "${@}"
+  install_packages "${@}"
+  place_configuration_files "${@}"
+  log 'info' 'Finished UDS setup process'
+}
 
 function purge_snapd() {
   command -v snap &>/dev/null || return 0
@@ -36,10 +85,8 @@ function purge_snapd() {
   killall snap
   systemctl stop snapd
 
-  until [[ $(snap list 2>&1 || :) == 'No snaps'*'installed'* ]]
-  do
-    while read -r SNAP _
-    do
+  until [[ $(snap list 2>&1 || :) == 'No snaps'*'installed'* ]]; do
+    while read -r SNAP _; do
       snap remove --purge "${SNAP}" &>/dev/null || :
     done < <(snap list |& tail -n +2 || :)
   done
@@ -56,31 +103,17 @@ function add_ppas() {
 
   wget -q -O /etc/apt/sources.list "${GITHUB_RAW_URL}apt/sources.list"
 
-  local PPA_SOURCES_FILES=(
-    'eza'
-    'git-core'
-    'neovim-unstable'
-  )
-
-  if ${INSTALL_GUI}
-  then
-    PPA_SOURCES_FILES+=(
-      'alacritty'
-      'cryptomator'
-      'mozillateam'
-      'regolith'
-      'vscode'
-    )
-  fi
+  local PPA_SOURCES_FILES=('eza' 'git-core' 'neovim-unstable')
+  ${INSTALL_GUI} && PPA_SOURCES_FILES+=('alacritty' 'cryptomator' 'mozillateam' 'regolith' 'vscode')
 
   local PPA_SOURCE_FILE
-  for PPA_SOURCE_FILE in "${PPA_SOURCES_FILES[@]}"
-  do
-    wget -q -O "/etc/apt/sources.list.d/${PPA_SOURCE_FILE}.sources" "${GITHUB_RAW_URL}apt/${PPA_SOURCE_FILE}.sources"
+  for PPA_SOURCE_FILE in "${PPA_SOURCES_FILES[@]}"; do
+    wget -q -O \
+      "/etc/apt/sources.list.d/${PPA_SOURCE_FILE}.sources" \
+      "${GITHUB_RAW_URL}apt/${PPA_SOURCE_FILE}.sources"
   done
 
-  if ${INSTALL_GUI}
-  then
+  if ${INSTALL_GUI}; then
     log 'debug' 'Overriding Firefox PPA priority to not use the Snap-package'
     cat >/etc/apt/preferences.d/mozilla-firefox << "EOM"
 Package: *
@@ -102,13 +135,10 @@ function install_packages() {
   apt-get -qq upgrade
   apt-get -qq dist-upgrade
 
-  log 'debug' 'Removing update manager'
-  apt-get -qq purge update-manager-core
-
   log 'debug' 'Removing no longer required packages'
   apt-get -qq autoremove
 
-  log 'debug' 'Installing packages now'
+  log 'debug' 'Installing packages'
   local PACKAGES=(
     'bash-completion'
     'bat'
@@ -126,8 +156,7 @@ function install_packages() {
     'xz-utils'
   )
 
-  if ${INSTALL_GUI}
-  then
+  if ${INSTALL_GUI}; then
     local PACKAGES+=(
       'alacritty'
       'code'
@@ -156,31 +185,28 @@ function install_packages() {
     )
   fi
 
-  log 'info' "Installing packages: ${PACKAGES[*]}"
-  if ! apt-get install -qq "${PACKAGES[@]}"
-  then
+  log 'debug' "Packages selected for installation: ${PACKAGES[*]}"
+  if ! apt-get install -qq "${PACKAGES[@]}"; then
     log 'error' 'Package installation was unsuccessful'
     exit 1
   fi
 
-  if ${INSTALL_GUI}
-  then
+  if ${INSTALL_GUI}; then
     log 'debug' 'Applying VS Code patch'
     local CODE_SOURCES_FILE='/etc/apt/sources.list.d/vscode.list'
-    if [[ -f ${CODE_SOURCES_FILE} ]]
-    then
+    if [[ -f ${CODE_SOURCES_FILE} ]]; then
       echo '# deb [arch=amd64] http://packages.microsoft.com/repos/code stable main' >"${CODE_SOURCES_FILE}"
     fi
 
-    log 'debug' 'Removing unwanted packages now'
+    log 'debug' 'Removing unwanted packages'
     apt-get -qq remove --purge i3xrocks regolith-i3xrocks-config
   fi
 
-  log 'debug' 'Removing unwanted packages now'
+  log 'debug' 'Removing unwanted packages'
   apt-get -qq autoremove
 
   log 'debug' 'Installing Starship prompt'
-  curl -sSfL https://starship.rs/install.sh | sh -s -- --force >/dev/null
+  wget -q -O- 'https://starship.rs/install.sh' | sh -s -- --force >/dev/null
 
   log 'info' 'To install ble.sh, visit https://github.com/akinomyoga/ble.sh'
 
@@ -194,16 +220,14 @@ function place_configuration_files() {
   echo "permit persist ${USER}" >/etc/doas.conf
   chown -c root:root /etc/doas.conf
   chmod -c 0400 /etc/doas.conf
-  if doas -C /etc/doas.conf
-  then
+  if doas -C /etc/doas.conf; then
     log 'debug' 'doas config looks good'
   else
     log 'warn' 'doas config has errors - do not use it immediately'
   fi
 
   log 'debug' 'Setting up user-specific configuration'
-  if ! cd "${HOME}"
-  then
+  if ! cd "${HOME}"; then
     log 'error' 'Could not change directory to home directory - cannot place configuration files'
     exit 1
   fi
@@ -220,8 +244,7 @@ function place_configuration_files() {
     '.config/nvim/init.lua'
   )
 
-  if ${INSTALL_GUI}
-  then
+  if ${INSTALL_GUI}; then
     readonly REGOLITH_DIR='.config/regolith3'
     CONFIG_FILES+=(
       '.config/alacritty/alacritty.toml'
@@ -241,14 +264,12 @@ function place_configuration_files() {
     )
   fi
 
-  for FILE in "${CONFIG_FILES[@]}"
-  do
+  for FILE in "${CONFIG_FILES[@]}"; do
     mkdir -p "$(dirname "${HOME}/${FILE}")"
     wget -q -O "${HOME}/${FILE}" "${GITHUB_RAW_URL}home/${FILE}"
   done
 
-  if ${INSTALL_GUI}
-  then
+  if ${INSTALL_GUI}; then
     sed -i "s|HOME|${HOME}|g" "${HOME}/${REGOLITH_DIR}/Xresources"
     log 'info' 'To change the bookmarks in Nautilus, edit ~/.config/user-firs.dirs, ~/.config/gtk-3.0/bookmarks, and /etc/xdg/user-dirs.defaults'
   fi
@@ -259,56 +280,4 @@ function place_configuration_files() {
   log 'debug' 'Finished placing configuration files'
 }
 
-function main() {
-  if [[ $(uname -m || :) != 'x86_64' ]]
-  then
-    log 'error' 'arm64 is not supported (APT sources are complicated for arm64)'
-    exit 1
-  fi
-
-  [[ ${*} == *--no-gui* ]] && INSTALL_GUI=false
-
-  if [[ ${EUID} -ne 0 ]]
-  then
-    touch "${TMP_CHECK_FILE}"
-
-    if ${INSTALL_GUI}
-    then
-      log 'debug' 'Running user-specific setup'
-      gsettings set org.freedesktop.ibus.panel.emoji hotkey "[]" || :
-    fi
-
-    log 'debug' 'Starting actual setup'
-    # shellcheck disable=SC2312
-    sudo env -                 \
-      USER="${USER}"           \
-      HOME="${HOME}"           \
-      LOG_LEVEL="${LOG_LEVEL}" \
-      bash "$(realpath -eL "${BASH_SOURCE[0]}")" "${@}"
-    exit
-  fi
-
-  if ${INSTALL_GUI}
-  then
-    log 'info' 'Not configuring GUI'
-  else
-    log 'info' 'Configuring GUI'
-  fi
-
-  if [[ ! -f ${TMP_CHECK_FILE} ]]
-  then
-    log 'error' 'Do not run this script as root yourself'
-    exit 1
-  fi
-
-  cd /tmp
-
-  log 'info' 'Starting UDS setup process'
-  #purge_snapd
-  add_ppas "${@}"
-  install_packages "${@}"
-  place_configuration_files "${@}"
-  log 'info' 'Finished UDS setup process'
-}
-
-main "${@}"
+__start "${@}"
